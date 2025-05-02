0a086aa5b130f7ff2d1cbd64a0bb5aa8f25b3ec7
ARJ <ar2004j@gmail.com>
2025-05-02 11:33:45 +0330

    player,staff,referee,team tables

--- federation.sql
+++ federation.sql
@@ -1,8 +1,40 @@
 CREATE TABLE person (
-    NationalIDOrPassport VARCHAR(20) NOT NULL,
-    Nationality VARCHAR(20) NOT NULL,
-    FirstNameAndLastName_English VARCHAR(50) NOT NULL,
-    FirstNameAndLastName_Persian VARCHAR(50) NOT NULL,
-    UNIQUE (NationalIDOrPassport , Nationality) ,
-    PRIMARY KEY (NationalIDOrPassport , Nationality)
-)
+    national_id_or_passport VARCHAR(20) NOT NULL,
+    nationality VARCHAR(20) NOT NULL,
+    firstname_and_lastName_english VARCHAR(50) NOT NULL,
+    firstname_and_lastname_persian VARCHAR(50) NOT NULL,
+    address TEXT NOT NULL,
+    PRIMARY KEY (national_id_or_passport , nationality)
+);
+CREATE TABLE player (
+    player_id SERIAL,
+    national_id_or_passport VARCHAR(20) NOT NULL,
+    nationality VARCHAR(20) NOT NULL,
+    free BOOL NOT NULL ,
+    FOREIGN KEY (national_id_or_passport,nationality) REFERENCES person(national_id_or_passport,nationality),
+    PRIMARY KEY (player_id)
+);
+CREATE TABLE staff (
+    staff_id SERIAL,
+    national_id_or_passport VARCHAR(20) NOT NULL,
+    nationality VARCHAR(20) NOT NULL,
+    post VARCHAR(20) NOT NULL ,
+    FOREIGN KEY (national_id_or_passport,nationality) REFERENCES person(national_id_or_passport,nationality),
+    PRIMARY KEY (staff_id,post)
+);
+CREATE TABLE referee (
+    referee_id SERIAL,
+    national_id_or_passport VARCHAR(20) NOT NULL,
+    nationality VARCHAR(20) NOT NULL,
+    level VARCHAR(20) NOT NULL ,
+    FOREIGN KEY (national_id_or_passport,nationality) REFERENCES person(national_id_or_passport,nationality),
+    PRIMARY KEY (referee_id)
+);
+
+CREATE TABLE team (
+    team_id SERIAL,
+    name VARCHAR(20) NOT NULL,
+    date_of_foundation DATE NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    PRIMARY KEY (team_id)
+);
